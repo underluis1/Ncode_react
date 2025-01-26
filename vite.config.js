30190146e53462dce92bc3f579c4b09323cc2f51
raff <raffaelelugiebllo@gmail.com>
2025-01-26 19:56:53 +0100

    Aggiornamento configurazione vite.config.js e dipendenze

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom'], // Specifica le dipendenze da escludere dal bundle
+    },
+  },
 })
